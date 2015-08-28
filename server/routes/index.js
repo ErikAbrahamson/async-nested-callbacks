2d7228dc833eee86405f1caaa8045305abd09f42
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-28 11:15:28 -0600

    refactor for simplicity

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var sentence = '', currentFile;
-var nextArg;
+var sentence = '', currentFile, nextArg;
+
+function divide(data) {
+  return data.split('\n');
+}
 
 fs.readFile('./files/start.txt', 'utf8', function(err, data) {
-  currentFile = data.split('\n');
+  currentFile = divide(data);
   sentence += currentFile[0] + ' ';
   nextArg = './files/' + currentFile[1];
   fs.readFile(nextArg, 'utf8', function(err, data) {
-    currentFile = data.split('\n');
+    currentFile = divide(data);
     sentence += currentFile[0] + ' ';
     nextArg = './files/' + currentFile[1];
     fs.readFile(nextArg, 'utf8', function(err, data) {
-      currentFile = data.split('\n');
-      sentence += currentFile[0];
+      sentence += data;
       console.log(sentence);
       return sentence;
     });
